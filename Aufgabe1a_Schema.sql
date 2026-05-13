d0b39fdd619d34dfeb5283e0fbfcdcd54e97e713
v13sem <viktoria-semenuk@outlook.com>
2026-05-13 08:24:42 +0200

    nochmal Update für Git

--- Aufgabe1a_Schema.sql
+++ Aufgabe1a_Schema.sql
@@ -11,76 +11,154 @@
 --uq_...   = Unique Constraint
 --idx_...  = Index
 
-CREATE TYPE product_type AS ENUM ('book','cd','dvd'); 
-CREATE TYPE contributor_rolle as ENUM ('Autor/in' , 'Künstler/in', 'Schauspieler/in', 'Filmemacher/in', 'Regisseur/in') ; 
+CREATE TYPE product_type_enum AS ENUM ('book','music_cd','dvd'); 
+CREATE TYPE contributor_rolle AS ENUM ('Autor/in' , 'Künstler/in', 'Schauspieler/in', 'Filmemacher/in', 'Regisseur/in') ; 
 
 CREATE TABLE product(
-    product_id VARCHAR(50) NOT NULL UNIQUE,
-    titel VARCHAR(255) NOT NULL,
-    rating INTEGER,
-    salesrank INTEGER,
-    picture VARCHAR(1000),
+    product_id      VARCHAR(50) NOT NULL, 
+    title           VARCHAR(255) NOT NULL,
+    product_type product_type_enum NOT NULL,
+    salesrank       INTEGER,
+    picture         VARCHAR(1000),
+    -- rating wird aus reviews berechnet, daher kein Attribut in der Produkt-Tabelle
     
-    CONSTRAIN pk_product PRIMARY KEY (product_id)
-    CONSTRAINT ck_rating CHECK (rating >= 0 AND rating <= 5),
-    CONSTRAINT ck_salesrank CHECK (salesrank > 0)
-)   
+    CONSTRAINT pk_product 
+        PRIMARY KEY (product_id),
+
+    CONSTRAINT ck_product_type 
+        CHECK (product_type IN ('book', 'music_cd', 'dvd')), -- eigentlich über enum definiert, aber sicherheitshalber nochmal check constraint
+
+    CONSTRAINT ck_product_salesrank 
+        CHECK (salesrank IS NULL OR salesrank > 0)
+);  
+
 --Mitwirkinde
 CREATE TABLE contributor(
-    contributor_id VARCHAR(20) PRIMARY KEY, 
-    contributor_name VARCHAR(200), 
-)
+    contributor_id      INTEGER  NOT NULL, 
+    contributor_name    VARCHAR(200) NOT NULL,
+
+    CONSTRAINT pk_contributor 
+        PRIMARY KEY (contributor_id),
+
+    CONSTRAINT ck_contributor_id 
+        CHECK (contributor_id > 0)
+);
 
 --Mitwirkende_produkt
 CREATE TABLE contributor_product(
-    product_id VARCHAR(20) PRIMARY KEY REFERENCES product(product_id),
-    contributor_id VARCHAR(20) PRIMARY KEY REFERENCES contributor(contributor_id), 
-    rolle contributor_rolle PRIMARY KEY, 
+    product_id VARCHAR(50) NOT NULL,
+    contributor_id INTEGER NOT NULL,
+    rolle contributor_rolle NOT NULL,
+
+    CONSTRAINT pk_contributor_product
+        PRIMARY KEY (product_id, contributor_id, rolle),
+
+    CONSTRAINT fk_contributor_product_product
+        FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT fk_contributor_product_contributor
+        FOREIGN KEY (contributor_id)
+        REFERENCES contributor (contributor_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
 ); 
 
 --Verlag
 CREATE TABLE publisher(
-    publisher_id INTEGER PRIMARY KEY CHECK(publisher_id>0),   --sollen wir so die unsigned int problematik lösen?
-    publisher_name VARCHAR(200), 
+    publisher_id INTEGER    NOT NULL,   
+    publisher_name VARCHAR(200) NOT NULL,
+    CONSTRAINT pk_publisher 
+        PRIMARY KEY (publisher_id),
+    CONSTRAINT ck_publisher_id
+        CHECK (publisher_id > 0)--sollen wir so die unsigned int problematik lösen? Finde ich gut!
 ); 
 
 CREATE TABLE book(
-    product_id VARCHAR(20) PRIMARY KEY REFERENCES produkt(product_id)
-    isbn VARCHAR(20) UNIQUE  --VARCHAR weil ISBN zeichen/buchstabe X enthalten können 
-    pages INTEGER CHECK (pages>0) 
-    publication_date DATE --kein Check, da auch zukünftige Buchveröffentlichungen angezeigt werden können 
-    publisher_id INTEGER REFERENCES publisher(publisher_id)
+    product_id VARCHAR(50) NOT NULL,
+    isbn VARCHAR(20) UNIQUE,  --VARCHAR weil ISBN zeichen/buchstabe X enthalten können 
+    pages INTEGER CHECK ( pages IS NULL OR pages>0), -- Seitenzahl kann auch unbekannt sein, daher NULL 
+    publication_date DATE, --kein Check, da auch zukünftige Buchveröffentlichungen angezeigt werden können 
+    publisher_id INTEGER NOT NULL,
+
+    CONSTRAINT pk_book
+        PRIMARY KEY (product_id),
+
+    CONSTRAINT fk_book_product
+        FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+        CONSTRAINT fk_book_publisher
+        FOREIGN KEY (publisher_id)
+        REFERENCES publisher (publisher_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
 ); 
 
 
 CREATE TABLE music_cd(
-    product_id VARCHAR(20) PRIMARY KEY REFERENCES produkt(product_id),
-    cd_track INTEGER,      --track numbers Wie hoch ist die durchschnittliche Anzahl von Liedern einer Musik-CD?
-    cd_name VARCHAR(200) PRIMARY KEY,
+    product_id VARCHAR(50) NOT NULL,
+    cd_name VARCHAR(200) NOT NULL, --könnte nicht funktionieren, wenn mehrere CDs den gleichen Namen haben, muss nicht unbedingt eindeutig sein 
     label VARCHAR(200), 
     publication_date DATE, 
+
+    CONSTRAINT pk_music_cd
+        PRIMARY KEY (product_id),
+
+    CONSTRAINT fk_music_cd_product
+        FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
 ); 
 
 -- cd Liste von Titeln, die durch ihren Namen gegeben sind.
+-- music_cd 1:n cd_title, da eine CD mehrere Titel haben kann, aber ein Titel nur zu einer CD gehört.
+          --track numbers Wie hoch ist die durchschnittliche Anzahl von Liedern einer Musik-CD?
+
+CREATE TABLE cd_title (
+    product_id VARCHAR(50) NOT NULL,
+    title_number INTEGER NOT NULL,
+    title_name VARCHAR(300) NOT NULL,
+
+    CONSTRAINT pk_cd_title
+        PRIMARY KEY (product_id, title_number),
+
+    CONSTRAINT fk_cd_title_music_cd
+        FOREIGN KEY (product_id)
+        REFERENCES music_cd (product_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT ck_cd_title_number
+        CHECK (title_number > 0)
+);
 
 CREATE TABLE dvd(
-    product_id VARCHAR(20) PRIMARY KEY REFERENCES produkt(product_id),
+    product_id VARCHAR(50) NOT NULL,
     format VARCHAR(100),
-    runtime_minutes VARCHAR(50), 
+    runtime_minutes INTEGER,
+    region_code VARCHAR(50) ,
+
+    CONSTRAINT pk_dvd
+        PRIMARY KEY (product_id),
+
+      CONSTRAINT fk_dvd_product
+        FOREIGN KEY (product_id)
+        REFERENCES product (product_id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+
+    CONSTRAINT ck_dvd_runtime
+        CHECK (runtime_minutes IS NULL OR runtime_minutes > 0) --falls unbekannt 
 );
 
 
 
-
-
-
-CREATE TABLE angebot (
-    produktnummer  INTEGER     REFERENCES produkt(product_id) #ON DELETE CASCADE,
-    filiale_id  INTEGER         REFERENCES filiale(filiale_id) #ON DELETE CASCADE,
-    preis       NUMERIC(10,2)   CHECK (preis > 0),  
-    zustand     VARCHAR(50)     CHECK (zustand IN ('neu', 'gebraucht', 'sehr gut', 'gut', 'akzeptabel')),
-    PRIMARY KEY (product_id, filiale_id)
-);
 
 --customer
 CREATE TABLE customer (
@@ -91,12 +169,15 @@ CREATE TABLE customer (
     
 --Filiale
 CREATE TABLE store (
-    store_id      INTEGER       NOT NULL, UNIQUE,
+    store_id      INTEGER       NOT NULL,
     store_name    VARCHAR(300)  NOT NULL,
-    address       VARCHAR(1000) NOT NULL,
+    store_address       VARCHAR(1000) NOT NULL,
 
     CONSTRAINT pk_store
-        PRIMARY KEY (store_id)
+        PRIMARY KEY (store_id),
+
+    CONSTRAINT ck_store_id
+        CHECK (store_id > 0)
 );
 -- Angebot
     CREATE TABLE offer (
@@ -122,31 +203,26 @@ CREATE TABLE store (
 
     CONSTRAINT chk_offer_price
         CHECK (price IS NULL OR price >= 0)
-    )
+    );
 -- price IS NOT NULL  => verfügbar
 -- price IS NULL      => nicht verfügbar
 
-CREATE TABLE customer (
-    customer_id    INTEGER       NOT NULL,
-    customer_name           VARCHAR(300)  NOT NULL,
 
-    CONSTRAINT pk_customer
-        PRIMARY KEY (customer_id)
-)
 -- Einkaufswagen
 CREATE TABLE cart (
-    cart_id INTEGER NOT NULL,
-    customer_id INTEGER NOT NULL,
-    cart_time TIMESTAMP NOT NULL,
+    cart_id         INTEGER NOT NULL,
+    customer_id     INTEGER NOT NULL,
+    cart_time       TIMESTAMP NOT NULL,
     shipping_address VARCHAR(1000) NOT NULL,
-    bank_account VARCHAR(100) NOT NULL,
+    bank_account    VARCHAR(100) NOT NULL,
+
     CONSTRAINT pk_cart PRIMARY KEY (cart_id),
     CONSTRAINT fk_cart_customer 
         FOREIGN KEY (customer_id) 
         REFERENCES customer (customer_id)  
         ON UPDATE CASCADE 
         ON DELETE RESTRICT
-)
+);
 --customer kann verschiedene Käufen; Lieferadressen; Kontodaten haben
 
 CREATE TABLE cartposition (
@@ -154,6 +230,7 @@ CREATE TABLE cartposition (
     product_id           VARCHAR(50)   NOT NULL,
     store_id             INTEGER       NOT NULL,
     price_at_purchase    DECIMAL(10,2) NOT NULL,
+    quantity             INTEGER       NOT NULL,
 
 
     CONSTRAINT pk_cartposition
@@ -176,10 +253,12 @@ CREATE TABLE cartposition (
 
     CONSTRAINT chk_cartposition_quantity
         CHECK (quantity > 0)
-)
+        
+);
+
 
 CREATE TABLE review (
-    review_id       INTEGER       NOT NULL,
+    review_id       VARCHAR(50)   NOT NULL,
     customer_id     INTEGER       NOT NULL,
     product_id      VARCHAR(50)   NOT NULL,
     rating          INTEGER       NOT NULL,
@@ -203,4 +282,4 @@ CREATE TABLE review (
 
     CONSTRAINT chk_review_rating
         CHECK (rating BETWEEN 1 AND 5)
-)
+);
