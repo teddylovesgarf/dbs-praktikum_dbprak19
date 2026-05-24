51d5e2bf7fec7bd2ee1ed494701d9162cad81d05
v13sem <viktoria-semenuk@outlook.com>
2026-05-24 16:49:58 +0200

    Update review and store loaders

--- src/main/java/loader/StoreLoader.java
+++ src/main/java/loader/StoreLoader.java
@@ -10,18 +10,18 @@ import parser.XMLParser;
 
 
 /*Ablauf
-1.XLM Datei parsen
-2. Root Elemant <shop> holen -> Daraus werden die Attribute name, street und zip gelesen.
-3. Attribute name, street, zip lesen -> z.B. <shop name="Dresden" street="Johann-Meyer-Straße" zip="01097">
+1.XML Datei parsen
+2. Root Element <shop> holen -> Daraus werden die Attribute name, street und zip gelesen.
+z.B. <shop name="Dresden" street="Johann-Meyer-Straße" zip="01097">
 4. Daten prüfen
-5. Store in die Datenabnk schreiben
+5. Store in die Datenbank schreiben
 6. Bei Fehlern: Fehlerprotokollierung mit ErrorLogger.logError() 
 */
 
 public class StoreLoader {
 
     private final Connection connection;
-    //Verbindung zur Datenbank wird im Konstruktor übergeben, damit sie von loadData() und insertReview() genutzt werden kann
+    //Verbindung zur Datenbank wird im Konstruktor übergeben, damit sie von loadData() und insertStore() genutzt werden kann
     //@param connection aktive Verbindung zur Datenbank
     public StoreLoader(Connection connection) {
         this.connection = connection;
@@ -29,56 +29,28 @@ public class StoreLoader {
 
     public void loadData(String filePath) {
         try {
+            //XML Datei öffnen und als Dokument speichern. Wenn die Datei nicht gelesen werden kann, wird eine IllegalArgumentException ausgelöst, die im Fehlerprotokoll festgehalten wird.
             Document doc = XMLParser.parse(filePath);
 
             if (doc == null) {
                 throw new IllegalArgumentException("XML Datei konnte nicht gelesen werden");
             }
-
+            // Das Root Element ist bei unseren Dateien das <shop> Element
             Element shop = XMLParser.getRoot(doc);
 
             String storeName = XMLParser.getAttribute(shop, "name");
             String street = XMLParser.getAttribute(shop, "street");
             String zip = XMLParser.getAttribute(shop, "zip");
 
-            validateStore(storeName, street, zip);
-            insertStore(storeName, street, zip);
-
-            ErrorLogger.incrementLoaded();
-
-        } catch (IllegalArgumentException e) {
-            ErrorLogger.logError(
-                    filePath,
-                    0,
-                    "store",
-                    "unbekannt",
-                    "",
-                    "Validierungsfehler",
-                    e.getMessage()
-            );
-
-        } catch (SQLException e) {
-            ErrorLogger.logError(
-                    filePath,
-                    0,
-                    "store",
-                    "Datenbank",
-                    "",
-                    "Datenbankfehler",
-                    "Message: " + e.getMessage()
-                            + " | SQLState: " + e.getSQLState()
-                            + " | ErrorCode: " + e.getErrorCode()
-            );
-        }
-    }
+            
 
     /*Prüft die Validität eines Store-Objekts anhand der folgenden Kriterien:
     name darf nicht leer sein
     street darf nicht leer sein
     zip darf nicht leer sein 
-    zip darf höchsten 10 Zeichen haben und muss eine gültige Postleitzahl sein
+    zip darf höchsten 10 Zeichen haben und muss nur Ziffern enthalten
     */
-    private void validateStore(String storeName, String street, String zip) {
+    
         if (storeName == null || storeName.isBlank()) {
             throw new IllegalArgumentException("store_name fehlt");
         }
@@ -98,10 +70,9 @@ public class StoreLoader {
         if (!zip.matches("\\d+")) {
             throw new IllegalArgumentException("zip enthält nicht nur Ziffern");
         }
-    }
+
 
         //Fügt einen neuen Store-Datensatz in die Datenbank ein. Wenn die Einfügung fehlschlägt, wird eine SQLException ausgelöst, die im Fehlerprotokoll festgehalten wird.
-    private void insertStore(String storeName, String street, String zip) throws SQLException {
         String sql = """
                 INSERT INTO store (store_name, street, zip)
                 VALUES (?, ?, ?)
@@ -109,19 +80,44 @@ public class StoreLoader {
 
         try (PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setString(1, storeName);
-            statement.setString(2, emptyToNull(street));
-            statement.setString(3, emptyToNull(zip));
+            statement.setString(2, street);
+            statement.setString(3, zip);
 
+            //Führt da SQL-Statement aus, um den neuen Store-Datensatz in die Datenbank einzufügen.
             statement.executeUpdate();
         }
-    }
-    //Macht leere Strings zu null, damit sie als NULL in die Datenbank geschrieben werden. Wenn der übergebene String null oder nur aus Leerzeichen besteht, wird null zurückgegeben. Ansonsten wird der String getrimmt zurückgegeben.
-    /*private String emptyToNull(String value) {
-        if (value == null || value.isBlank()) {
-            return null;
-        }
+        //Wenn alles geklappt hat, wird ein geladener Datensatz gezählt, damit am Ende die Anzahl der erfolgreich geladenen Datensätze ausgegeben werden kann.
+        ErrorLogger.incrementLoaded();
 
-        return value.trim();
+
+        }catch (IllegalArgumentException e) {
+            //Fehler bei fehlenden oder ungültigen XML Werten
+            ErrorLogger.logError(
+                filePath,
+                0,
+                "store",
+                "unbekannt",
+                "",
+                "Validierungsfehler",
+                e.getMessage()
+            );
+
+        } catch(SQLException e) {
+            //Fehler beim Einfügen in die Datenbank
+            ErrorLogger.logError(
+                filePath,  //Wo ist der Fehler passiert?
+                0,  //welche Zeile? Bei XML unbekannt, daher 0
+                "store", // Welche Entität? -> store
+                "Datenbank", //Welches Attribut? -> Fehler beim Einfügen in die Datenbank
+                "", //welche Rohdaten? -> keine, da Fehler beim Einfügen in die Datenbank
+                "Datenbankfehler", //welche Art von Fehler? -> Datenbankfehler
+                "Message: " + e.getMessage() //genaue Fehlerbeschreibung
+                            + " | SQLState: " + e.getSQLState()
+                            + " | ErrorCode: " + e.getErrorCode()
+            );
+        }
     }
-        */
 }
+
+   
+        
