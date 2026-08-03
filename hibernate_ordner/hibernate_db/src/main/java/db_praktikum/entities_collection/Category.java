70330a2e876ff850ba2d9d1c4c9560508b0a3669
V13sem <viktoria-semenuk@outlook.com>
2026-08-03 15:28:36 +0200

    category class

--- hibernate_ordner/hibernate_db/src/main/java/db_praktikum/entities_collection/Category.java
+++ hibernate_ordner/hibernate_db/src/main/java/db_praktikum/entities_collection/Category.java
@@ -1,10 +1,16 @@
 package db_praktikum.entities_collection;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
